08a77ffd78f8fbce1d4fdfd3a817bf181b13629c
Faizullah-Laghari <faizullahls123@gmail.com>
2024-01-24 11:07:55 +0500

    initial commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -25,7 +25,7 @@ import Wrapper from "../layout/wrapper";
 const index = () => {
   return (
     <Wrapper>
-      <SEO pageTitle={"Softec - Data analytics"} />
+      <SEO pageTitle={"Swift - Business"} />
       <HomeFive />
     </Wrapper>
   );
